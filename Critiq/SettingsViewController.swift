49d9d08ebaa157b3a2f9936532501b37cacf033a
Julian Gindi <juliangindi@gmail.com>
2015-03-08 16:07:23 -0400

    adding basis for settings view

---
+++ Critiq/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  Critiq
+//
+//  Created by _ on 3/8/15.
+//  Copyright (c) 2015 JulianGindi. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsViewController: NSView {
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
