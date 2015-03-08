ccff1dbcdf043f4837e3264a80d4b420194a6b76
Julian Gindi <juliangindi@gmail.com>
2015-03-08 16:30:29 -0400

    beginnings of a settings controller

--- Critiq/SettingsViewController.swift
+++ Critiq/SettingsViewController.swift
@@ -2,14 +2,19 @@
 //  SettingsViewController.swift
 //  Critiq
 //
-//  Created by _ on 3/8/15.
-//  Copyright (c) 2015 JulianGindi. All rights reserved.
+//  Created by Thomas Degrey and Julian Gindi on 3/8/15.
+//  Copyright (c) 2015 Thomas Degry and Julian Gindi. All rights reserved.
 //
 
 import Cocoa
 
 class SettingsViewController: NSView {
+    @IBAction func durationChange(sender: AnyObject) {
+    }
 
+    @IBAction func languageChange(sender: AnyObject) {
+    }
+    
     override func drawRect(dirtyRect: NSRect) {
         super.drawRect(dirtyRect)
 
